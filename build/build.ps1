338df24936f2962349ceee737414f162242a35c4
majronix <majron-w@yandex.ru>
2016-09-08 23:41:19 +0300

    Add files via upload

---
+++ build/build.ps1
@@ -0,0 +1,6 @@
+﻿$sourceCodeDir = (Get-ChildItem Env:BUILD_SOURCESDIRECTORY).Value
+$wwwRootDir = Join-Path -Path $sourceCodeDir -ChildPath "www"
+$outputFile = Join-Path -Path $sourceCodeDir -ChildPath "build\www.zip"
+
+Add-Type -A System.IO.Compression.FileSystem
+[IO.Compression.ZipFile]::CreateFromDirectory($wwwRootDir, $outputFile)
