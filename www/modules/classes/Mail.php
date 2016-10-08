e547d8d77d4dd7671315119dbcbd5eac989d2eb8
ITS-Zah <96andriy@ukr.net>
2016-10-08 18:38:11 +0300

    Update Mail.php

--- www/modules/classes/Mail.php
+++ www/modules/classes/Mail.php
@@ -1,141 +1,85 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * Date: 15.02.16
+ * Time: 17:54
+ */
 
 namespace Modules;
 
-require_once('../../../vendor/autoload.php');
-require_once('../../../settings/conf.php');
-require_once('../../../settings/connect.php');
-require_once('../../../settings/functions.php');
-require_once('../../../settings/main.php');
+use SendGrid;
 
-bd_session_start();
+/**
+ * Class Mail.
+ * Відправляє email використовуючи SendGrid.
+ * @package Modules
+ */
+class Mail
+{
+    private static $sendGrid;
 
-$db = $glb['mysqli'];
+    /**
+     * @param string $to
+     * @param string $subject
+     * @param string $html
+     * @param string $from
+     * @return bool
+     * @throws SendGrid\Exception
+     */
+    public static function send($to, $subject, $html, $from = 'info@makewear.com.ua', $name = 'Makewear')
+    {
+        if (null === self::$sendGrid) {
 
-require_once("../../phpmailer/PHPMailerAutoload.php");
+            //якщо екземпляр класу SendGrid не створений то створюєм
+            self::$sendGrid = new SendGrid('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');
+        }
+        $email = new SendGrid\Email();
+        $email
+            ->addTo($to)
+            ->setFrom($from)
+            ->setFromName($name)
+            ->setSubject($subject)
+            ->setHtml($html);
 
+        $result = self::$sendGrid->send($email);
 
-	if(isset($_GET['id'])){
-		$id=$_GET['id'];
+        if ($result->getCode() === 200) {
+            return false;
+        }
 
-		$result=$db -> query(<<<QUERY1
-				SELECT `com_id`,`cont_mail`
-				FROM `brenda_contact`
-				WHERE `com_id` ={$id};
-QUERY1
-		);
+        return true;
+    }
 
-		$row = $result->fetch_assoc();
+    public static  function sendMultiple($to, $subject, $html, $fromEmail  = 'info@makewear.com.ua', $fromName = 'Makewear')
+    {
+        if (null === self::$sendGrid) {
 
-		echo str_replace("0;","",$row['cont_mail']);
-	}
-	if(isset($_POST['where'])){
-		$to=$_POST['where'];
-		$whom=$_POST['whom'];
-		$subject=$_POST['sub'];
-		$massega=$_POST['txt'];
+            //якщо екземпляр класу SendGrid не створений то створюєм
+            self::$sendGrid = new SendGrid(SEND_GRID_KEY);
+        }
 
+        $names = array();
+        $emails = array();
+        $email = new SendGrid\Email();
 
-//		$mail = new PHPMailer;
-//
-//		//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-//
-//		$mail->isSMTP();                                      // Set mailer to use SMTP
-//		$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
-//		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-//		$mail->Username = 'sales@makewear.com.ua';                 // SMTP username
-//		$mail->Password = 'zoond363636';                           // SMTP password
-//		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-//		$mail->Port = 587;                                    // TCP port to connect to
-//		$mail->CharSet = "UTF-8";
-//		$mail->setFrom($whom, 'MakeWear');
-//		$mail->addAddress($to);     // Add a recipient
-//		$mail->isHTML(true);                              
-//
-//
-//		$mail->Subject = $subject;
-//		$mail->Body    = $massega;
-//
-//
-//		$massega=str_replace("'", "\'", $massega);
-//		$massega=str_replace('"', '\"', $massega);
+        foreach ($to as $item) {
+            $emails[] = $item['email'];
+            $names[] = $item['name'];
+        }
 
-		//$flag=0;
-                    
-		if(Mail::send($to, $subject, $massega)) {
-		    echo "Message could not be sent.\n";
-		    echo 'Mailer Error: ' . $mail->ErrorInfo."\n";
-		} else {
-		   // echo "Message has been sent\n";
-		   	echo "Отправил в {$to}";
-		   //	$flag=1
+        $email
+            ->setSmtpapiTos($emails)
+            ->setFrom($fromEmail)
+            ->setFromName($fromName)
+            ->setSubject($subject)
+            ->setHtml($html);
 
-			$today = date("d.m.Y H:i:s");
-			 
-			if($_POST["status_ob"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `sup_group` SET `sent_email`=`sent_email`+1, `sent_date`='{$today}' WHERE `group_id`='{$_POST["id"]}';
-QUERY1
-	);
-			}
-			if($_POST["status_oc"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `shop_orders` SET `sent_mail`=`sent_mail`+1, `sent_mail_save`='{$massega}'  WHERE `id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			if($_POST["status_oc"]==2){
-			 	$db->query(<<<QUERY1
-        		UPDATE `shop_orders` SET `sent_mail_client`='{$massega}'  WHERE `id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			if($_POST["deli_sent"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `sup_group` SET `delivery_sent`=`delivery_sent`+1 WHERE `group_id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			if($_POST["payment_dd"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `sup_group` SET `payment_sent_mail`=`payment_sent_mail`+1 WHERE `group_id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			if($_POST["payment_k"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `shop_orders` SET `payment_sent_mail`=`payment_sent_mail`+1, `payment_old_price`='{$_POST["price"]}' WHERE `id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			 
-			if($_POST["sent_mw_k"]==1){
-			 	$db->query(<<<QUERY1
-        		UPDATE `shop_orders` SET `sent_mail_mw_k`=`sent_mail_mw_k`+1 WHERE `id`='{$_POST["id"]}';
-QUERY1
-    );
-			}
-			//echo $massega;
+        $result = self::$sendGrid->send($email);
 
-// 			$idNew=$_POST["id"];
-// 			echo " - ".$idNew." ";
-// 			if(strpos($idNew,"_")!==false){
-// 				$so=explode("_",$idNew);
-// 				echo " ".$so[0].":".$so[1];
-// 				if($_POST["status_ob"]==1){
-// 			 		$db->query(<<<QUERY1
-//         		dbplus_update(relation, old, new) `shop_order_supplier` SET `sent_email`=1 WHERE `order_id`='{$so[0]}' AND `supplier_name_id`='{$so[1]}'; 
-// QUERY1
-//     );
-// 				}
-// 				if($_POST["payment_dd"]==1){
-// 				 	$db->query(<<<QUERY1
-//         			UPDATE `shop_order_supplier` SET `payment_sent_mail`=1 WHERE `order_id`='{$so[0]}' AND `supplier_name_id`='{$so[1]}';
-// QUERY1
-//     );
-// 				}
-// 			}
-		}
-	}
+        if ($result->getCode() === 200) {
+            return true;
+        }
 
-?>
+        return false;
+    }
+}
