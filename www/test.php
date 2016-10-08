d5ffa6e046c3e23c0ee61ba17e1c34309b42c8db
ITS-Zah <96andriy@ukr.net>
2016-10-08 18:52:23 +0300

    Update test.php

--- www/test.php
+++ www/test.php
@@ -1,1 +1,16 @@
-<? phpinfi("heloo!!!"); ?>
+<?php
+//require_once 'C:\OpenServer\domains\localhost\dumphper.php';
+//require_once 'C:\OpenServer\domains\MakeWear5\www\modules\classes\BlobStorage.php';
+
+
+
+
+//$blobStorage = new BlobStorage();
+
+ //   dump($blobStorage);
+
+
+
+
+echo phpinfo();
+
