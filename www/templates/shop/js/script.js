acfc4b9973aec8ff99f59cb205c9305d5e4d9102
ITS-Zah <96andriy@ukr.net>
2016-10-11 15:23:18 +0300

    Підготовка до злиття.

--- www/templates/shop/js/script.js
+++ www/templates/shop/js/script.js
@@ -2888,3 +2888,27 @@ $(document).ready(function(){
     });
     
 });
+/****** Кнопка ВВЕРХ */
+$(function() {
+                 
+    $(window).scroll(function() {
+     
+    if($(this).scrollTop() != 0) {
+     
+    $('#toTop').fadeIn();
+     
+    } else {
+     
+    $('#toTop').fadeOut();
+     
+    }
+     
+    });
+     
+    $('#toTop').click(function() {
+     
+    $('body,html').animate({scrollTop:0},800);
+     
+    });
+     
+});
