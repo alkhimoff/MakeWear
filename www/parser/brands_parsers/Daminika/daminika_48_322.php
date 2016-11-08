224d0effb97039b30160fc3f20fd2eb22d2c5301
Dyachuk Sergey <dyachukserge@gmail.com>
2016-11-08 16:27:01 +0200

    Daminika price2 & logo

--- www/parser/brands_parsers/Daminika/daminika_48_322.php
+++ www/parser/brands_parsers/Daminika/daminika_48_322.php
@@ -2,7 +2,6 @@
 //==============================================================================
 //			Daminika    48-322        		
 //============================================================================== 
-//http://mwdev/parser/reports/07_Nov/catid_322_verify.html
 //Переменные для записи в БД по умолчанию
 $existProd     = TRUE;
 $deleteProd    = FALSE;
@@ -16,45 +15,11 @@ $nameProd      = "";
 $descProd      = "";
 $existDub      = FALSE;
 $duplicateProd = "";
-/*
-             //Подготовка переменных товара из парсинга для записи в бд
-            parse:
-            $comExist         = $resultParsArray['exist'];
-            $price            = $resultParsArray["price"];
-            $price2           = $resultParsArray["price2"];
-            $comSizes         = $resultParsArray["sizes"];
-            $comOptions       = $resultParsArray["options"];
-            $comCount         = $resultParsArray['count'];
-            $code             = $resultParsArray['cod'];
-            $comName          = $resultParsArray['name'];
-            $comFullDesc      = $resultParsArray['desc'];
-            $comDuplicate     = $resultParsArray['existDub'][1];
- */
-/*
-    "cod" => $codProd, 
-    "name" => $nameProd, 
-    "exist" => $existProd,
-    "price" => $price,
-    "price2" => $price2, 
-        "sizes" => $sizesProd, 
-        "options" => $optionsProd, 
-        'count' => $comCount,
-        'desc' => $descProd,
-    'existDub' => array($existDub, $duplicateProd));
-*/
-//var_dump($curLink);
-//Exist URL
-/* if ($verify !== "import" && ($statusCode == 404)) {
-  $deleteProd = TRUE;
-  return;
-  } */
 
-foreach ($saw->shop->offers->offer as $key => $offer) { // ~280
-
+foreach ($saw->shop->offers->offer as $key => $offer) {
     $url = (string) $offer->url;
     
     if ($url == $curLink) {
-
         $imgMain = (string) $offer->picture[0];
         foreach ($offer->picture as $key => $value) {
             $imgDop      = (string) $value;
@@ -63,16 +28,17 @@ foreach ($saw->shop->offers->offer as $key => $offer) { // ~280
         
         $nameProd  = $offer->name."";
         $codProd = $offer->vendorCode."";
-        $price = $offer->price."";
-        //$optionsProd   = "optionsProd/options";
-        //$comCount      = "comCount/count";
+        
+        $price = $offer->price."";      
+        $proc = ceil($price / 100 * $_SESSION['per']);
+        $price2 = ceil($price + $proc);
 
         $arrayDesc  = deleteEmptyArrDescValues(explode("<p>", $offer->description));
         array_pop($arrayDesc);
 
         if (isset($arrayDesc)) {
-           // $tmp = array_shift($arrayDesc);
             $searchArray = array('ткань:', 'длина изделия:', "длина рукава:");
+            
             foreach ($arrayDesc as $key => $value) {
                 $value = substr($value, 0, strpos($value, "</p>"));
                 
@@ -80,12 +46,12 @@ foreach ($saw->shop->offers->offer as $key => $offer) { // ~280
                     $descTmp = $value;
                     continue;
                 }  
+                
                 $descProd = findStringDesc($value, $searchArray, $descProd);
                 
                 if (count($arrayDesc) == $key) {
                     $value = substr($value, 0, strpos($value, "<table"));
                 }
-
                 if (count($arrayDesc) == $key + 1) {
                     $descProd .= "<p><span>На фото размер:</span>".$offer->param[0]."</p>";
                     $descProd .= "<p><span>На фото рост модели:</span>".$offer->param[1]."</p>";
@@ -93,7 +59,7 @@ foreach ($saw->shop->offers->offer as $key => $offer) { // ~280
                 }
             }
         }
-    
+        
     } else {
         continue;
     }
@@ -109,7 +75,8 @@ foreach ($saw->shop->offers->offer as $key => $offer) { // ~280
 //==============================================================================
 //                   Если это проверщик то выходим из скрипта
 //==============================================================================
-
+/*
     if ($verify == "verify") {
         return;
     }
+*/
